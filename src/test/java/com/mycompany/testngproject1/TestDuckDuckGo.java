9435fd532315c923184d2529fe1288f49ed4e6ce
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-09-05 09:33:33 -0500

    Add files via upload

---
+++ src/test/java/com/mycompany/testngproject1/TestDuckDuckGo.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testngproject1;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+/**
+ *
+ * @author baur
+ */
+public class TestDuckDuckGo {
+    
+    private WebDriver driver;
+    
+    DuckDuckGoPage searchPage;
+    
+    DuckDuckGoResultPage resultPage;
+    
+    @BeforeTest
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().setSize(new Dimension(1366, 768));
+    }
+    
+    @AfterTest
+    public void tearDown() {
+        if (driver != null)
+            driver.close();
+    }
+    
+    @Test
+    public void testDuckDuckGo() {
+        driver.get("https://www.duckduckgo.com");
+        
+        searchPage = new DuckDuckGoPage(driver);
+        searchPage.setSearch("DuckDuckGo");
+        searchPage.clickSearch(); // OR searchPage.button.click();
+        
+        resultPage = new DuckDuckGoResultPage(driver);
+        
+        Assert.assertFalse(resultPage.getResults().isEmpty());
+    }
+}
